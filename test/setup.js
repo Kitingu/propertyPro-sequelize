c233373aa9ff5a69209fc1a355be1800bd4fa5f4
benlegendj <benlegendj@gmail.com>
2019-09-05 22:01:00 +0300

    add unit test for app functionality

---
+++ test/setup.js
@@ -0,0 +1,71 @@
+export default {
+  user: {
+    firstname: "benedict",
+    lastname: "gathee",
+    email: "asdf@gmail.com",
+    password: "asdQ@123",
+    phoneNumber: "4949494921",
+    isAgent: true
+  },
+  user1: {
+    firstname: "nelson",
+    lastname: "mandela",
+    email: "nelson@gmail.com",
+    password: "asdQ@123",
+    phoneNumber: "4949494921",
+    isAgent: false
+  },
+  user2: {
+    firstname: "charles",
+    lastname: "asdf",
+    email: "admin@gmail.com",
+    password: "asdQ@123",
+    phoneNumber: "0715357896",
+    isAgent: true
+  },
+  userLogin1: {
+    email: "asdf@gmail.com",
+    password: "asdQ@123"
+  },
+  invalid_login: {
+    email: "asdmail.com",
+    password: "asdQ@123"
+  },
+  sample_property: {
+    status: "goog",
+    state: "kenya",
+    city: "nairobi",
+    type: "two",
+    price: "40000",
+    address: "3943",
+    image_url:
+      "http://res.cloudinary.com/benlegendj/image/upload/v1561843898/property/iuaqakp4nrm8tzccsddo.png",
+    owner: "asdf@gmail.com"
+  },
+  sample_property1: {
+    propertyId: 3,
+    status: "goog",
+    state: "kenya",
+    city: "nairobi",
+    type: "two",
+    price: "40000",
+    address: "3943",
+    image_url:
+      "http://res.cloudinary.com/benlegendj/image/upload/v1561843898/property/iuaqakp4nrm8tzccsddo.png",
+    owner: "benlegendj@gmail.com"
+  },
+  newPrice: {
+    price: "12048"
+  },
+  invalidPrice: {
+    price: "sdfghjk"
+  },
+  flag: {
+    reason: "price",
+    description: "the price is very high"
+  },
+  invalidFlag: {
+    reason: "",
+    description: "the price is very high"
+  }
+};
---
+++ test/test-app.js
@@ -0,0 +1,48 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+import utils from "./setup";
+import app  from "../app";
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe("test server", () => {
+  it("handle 404 error", done => {
+    chai
+      .request(app)
+      .patch("/api/v2/prope")
+      .send(utils.flag)
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.body.error).equals("Resource not found");
+        expect(res).to.have.status(404);
+        done();
+      });
+  });
+
+  it("handle 405 error", done => {
+    chai
+      .request(app)
+      .put("/api/v2/property")
+      .send(utils.flag)
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.body.error).equals("This method is not allowed");
+        expect(res).to.have.status(405);
+        done();
+      });
+  });
+
+  it("handle get by non integer", done => {
+    chai
+      .request(app)
+      .get("/api/v2/property/abc")
+      .send(utils.flag)
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res.body.error).equals("property id should only be a number");
+        expect(res).to.have.status(400);
+
+        done();
+      });
+  });
+});
